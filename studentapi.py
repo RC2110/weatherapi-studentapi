82ff1b46a18f027b7aff7a10f06c8c2326f3f446
Rajaa C <96713906+RC2110@users.noreply.github.com>
2023-09-30 21:16:06 +0530

    Worked on dictionary and fixed the dict irrespective of case sensitive words.

--- studentapi.py
+++ studentapi.py
@@ -7,9 +7,19 @@ def home():
     return render_template("task.html")
 
 @app.route('/api/hb1/<checkword>/')
-def capital(checkword):
+def meaning(checkword):
+    import pandas as pd
+    df = pd.read_csv("dataset/dictionary.csv")
+
+    if df.loc[df['word'] == checkword].empty:
+        checkword = checkword.title()
+        if df.loc[df['word'] == checkword].empty:
+            checkword = checkword.lower()
+
+    mean1 = df.loc[df['word'] == checkword]['definition'].squeeze()
+
     return {'word': checkword,
-            'nword':checkword.upper()
+            'nword':mean1
             }
 
 if __name__ == "__main__":
